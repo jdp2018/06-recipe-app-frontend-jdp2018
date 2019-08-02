1115e7a10402bac32bd2230170188f0ee11f1584
Diego Pinones <pinones.diego@gmail.com>
2019-08-02 15:33:01 +0200

    added some more styling

--- angular-recipe/src/app/categories/categories.component.ts
+++ angular-recipe/src/app/categories/categories.component.ts
@@ -1,34 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  selector: "app-categories",
+  templateUrl: "./categories.component.html",
+  styleUrls: ["./categories.component.scss"]
 })
 export class CategoriesComponent implements OnInit {
-/*
+  /*
   categories: category[];
   selectedCategory: category;
   */
   meals: meal[];
   selectedMeals: meals;
+  id: number;
 
   onSelect(meals: meals): void {
     this.selectedMeals = meals;
     console.log(this.selectedMeals);
   }
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {}
 
   ngOnInit() {
     this.data.getRecipe().subscribe((resp: meals) => {
       this.meals = resp.meals;
     });
   }
- 
 }
-
-
--- angular-recipe/src/app/global.d.ts
+++ angular-recipe/src/app/global.d.ts
@@ -22,6 +22,21 @@ interface meal {
     strTags: null;
     strYoutube: string;
     strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4: string;
+    strIngredient5: string;
+    strIngredient6: string;
+    strIngredient7: string;
+    strIngredient8: string;
+    strIngredient9: string;
+    strIngredient10: string;
+    strIngredient11: string;
+    strIngredient12: string;
+    strIngredient13: string;
+    strIngredient14: string;
+    strIngredient15: string;
+
     
 }
 interface meals {
